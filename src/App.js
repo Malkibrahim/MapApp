a95573faa19e929fe1c1fa34cc968205d5cc5e46
Malak Ibrahim <malak.ibrahim@smart.local>
2020-11-03 12:18:18 +0200

    colored dots

--- src/App.js
+++ src/App.js
@@ -25,8 +25,8 @@ export class WebMapView extends React.Component {
       this.view = new MapView({
         container: this.mapRef.current,
         map: map,
-        center: [31.2105881, 30.0282734],
-        zoom: 8,
+        center: [30.0282734, 31.2105881],
+        zoom: 13,
       });
       var graphicsLayer = new GraphicsLayer();
       map.add(graphicsLayer);
@@ -36,34 +36,211 @@ export class WebMapView extends React.Component {
           longitude: i.y_coordinate,
           latitude: i.x_coordinate,
         };
+        if (i.y_coordinate <= 30) {
+          var simpleMarkerSymbol = {
+            type: "simple-marker",
+            color: [0, 150, 50],
+            outline: {
+              color: [255, 255, 255],
+              width: 2,
+            },
+            size: 10,
+          };
+        } else if (i.y_coordinate >= 30) {
+          var simpleMarkerSymbol = {
+            type: "simple-marker",
+            color: "#aa3a3a",
+            outline: {
+              color: [255, 255, 255],
+              width: 2,
+            },
+            size: 10,
+          };
+        }
 
-        var simpleMarkerSymbol = {
-          color: [0, 150, 50],
-          outline: {
-            color: "#f6f5f1",
-            width: 1,
-          },
-        };
         var attributes = {
-          Name: "My point",
+          Name: "" + "governorate code : " + i.governorate_code + "",
           Location: " Point Dume State Beach",
         };
-
         var popupTemplate = {
-          title: "{ Name}",
-          content: "" + i.damen_merchant_code + "",
+          title: "{Name}",
+          content: "" + "merchant code : " + i.damen_merchant_code + "",
         };
 
         var pointGraphic = new Graphic({
           geometry: point,
           symbol: simpleMarkerSymbol,
-
           attributes: attributes,
           popupTemplate: popupTemplate,
         });
 
+        // var pointGraphic = new Graphic({
+        //   geometry: point,
+        //   symbol: simpleMarkerSymbol,
+        // });
+
         graphicsLayer.add(pointGraphic);
       });
+
+      // var simpleLineSymbol = {
+      //   //*** UPDATE ***//
+      //   color: [255,0,0],
+      //   width: 2,
+      //   //*** ADD ***//
+      //   style: "dash"
+      // };
+
+      // var polyline = {
+      //   type: "polyline",
+      //   paths: [
+      //     [31.2105881, 30.0282734],
+      //     [31.2409168, 29.9863887],
+      //     [31.210583, 30.0282015],
+      //   ],
+      // };
+
+      // var polylineGraphic = new Graphic({
+      //   geometry: polyline,
+      //   symbol: simpleLineSymbol,
+      // });
+
+      // graphicsLayer.add(polylineGraphic);
+      // var polygon = {
+      //   type: "polygon",
+      //   rings: [
+      //     [31.2105881, 30.0282734],
+      //     [31.2409168, 29.9863887],
+      //     [31.210583, 30.0282015],
+      //   ],
+      // };
+
+      // var simpleFillSymbol = {
+      //   // *** UPDATE ***//
+      //   color: [50,100,255,.5],
+      //   outline: {
+      //     color: [50, 100, 255],
+      //     width: 1
+      //   },
+      //   //*** ADD ***//
+      //   style: "backward-diagonal"
+      // };
+
+      // var polygonGraphic = new Graphic({
+      //   geometry: polygon,
+      //   symbol: simpleFillSymbol,
+      // });
+
+      // graphicsLayer.add(polygonGraphic);
+      //
+      // var trailheadsRenderer = {
+      //   type: "simple",
+      //   symbol: {
+      //     type: "picture-marker",
+      //     url:
+      //       "http://static.arcgis.com/images/Symbols/NPS/npsPictograph_0231b.png",
+      //     width: "18px",
+      //     height: "18px",
+      //   },
+      // };
+      // var trailheadsLabels = {
+      //   symbol: {
+      //     type: "text",
+      //     color: "#FFFFFF",
+      //     haloColor: "#5E8D74",
+      //     haloSize: "2px",
+      //     font: {
+      //       size: "12px",
+      //       family: "Noto Sans",
+      //       style: "italic",
+      //       weight: "normal",
+      //     },
+      //   },
+      //   labelPlacement: "above-center",
+      //   labelExpressionInfo: {
+      //     expression: `${items[0].governorate_code}`,
+      //   },
+      // };
+      // var trailheads = new FeatureLayer({
+      //   url:
+      //     "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trailheads/FeatureServer/0",
+      //   renderer: trailheadsRenderer,
+      //   labelingInfo: [trailheadsLabels],
+      // });
+      // var trailsRenderer = {
+      //   type: "simple",
+      //   symbol: {
+      //     color: "#BA55D3",
+      //     type: "simple-line",
+      //     style: "solid",
+      //   },
+      //   visualVariables: [
+      //     {
+      //       type: "size",
+      //       field: "ELEV_GAIN",
+      //       minDataValue: 0,
+      //       maxDataValue: 2300,
+      //       minSize: "3px",
+      //       maxSize: "7px",
+      //     },
+      //   ],
+      // };
+      // var trails = new FeatureLayer({
+      //   url:
+      //     "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trails/FeatureServer/0",
+      //   renderer: trailsRenderer,
+      //   opacity: 0.75,
+      // });
+      // var bikeTrailsRenderer = {
+      //   type: "simple",
+      //   symbol: {
+      //     type: "simple-line",
+      //     style: "short-dot",
+      //     color: "#FF91FF",
+      //     width: "1px",
+      //   },
+      // };
+      // var bikeTrails = new FeatureLayer({
+      //   url:
+      //     "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trails/FeatureServer/0",
+      //   renderer: bikeTrailsRenderer,
+      //   definitionExpression: "USE_BIKE = 'YES'",
+      // });
+
+      // map.add(trails, 0);
+      // map.add(trailheads);
+      // map.add(bikeTrails, 1);
+      // function createFillSymbol(value, color) {
+      //   return {
+      //     value: value,
+      //     symbol: {
+      //       color: color,
+      //       type: "simple-fill",
+      //       style: "solid",
+      //       outline: {
+      //         style: "none",
+      //       },
+      //     },
+      //     label: value,
+      //   };
+      // }
+
+      // var openSpacesRenderer = {
+      //   type: "unique-value",
+      //   field: "TYPE",
+      //   uniqueValueInfos: [
+      //     createFillSymbol("Natural Areas", "#9E559C"),
+      //     createFillSymbol("Regional Open Space", "#A7C636"),
+      //     createFillSymbol("Local Park", "#149ECE"),
+      //     createFillSymbol("Regional Recreation Park", "#ED5151"),
+      //   ],
+      // };
+      // var openspaces = new FeatureLayer({
+      //   url:
+      //     "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Parks_and_Open_Space/FeatureServer/0",
+      //   renderer: openSpacesRenderer,
+      //   opacity: 0.2,
+      // });
+      // map.add(openspaces, 0);
     });
   }
 
